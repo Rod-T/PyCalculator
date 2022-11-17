0003dead118886972857d590366c1dd69ee02adb
Rodrigo Tavares <93211615+Rod-T@users.noreply.github.com>
2022-11-17 22:11:50 +0000

    Update main.py
    updated info

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import re
 # Author: Rodrigo Tavares
 # Date: 17/11/2022
 # Title: Calculator
-# Version: 1.0
+# Version: 1.4
 # This program is a simple calculator that can calculate the square root of any number aswell as any other simple operations
+# If you want to use decimals use (.) and not (,)
 print(f"Welcome to the python calculator\nType esc if you want to exit\n")
 
 while 1:
@@ -15,6 +16,6 @@ while 1:
         print("Goodbye!...")
         break
     elif re.search("\Asqrt\(\d+\)|((\d+)(\+|-|\/|\*))+(\d+)|\Aesc", calc):
-        print(calc + "=" + str(eval(calc)))
+        print(calc + " = " + str(eval(calc)))
     else:
         print(f"Invalid calculation, try again or write 'esc' to exit")
